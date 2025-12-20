89655cee251a4298266b8e1fda23904b27fbbf90
Sahhana <sahhana13@gmail.com>
2025-12-20 15:32:23 +0530

    Add files via upload

---
+++ Foods.java
@@ -0,0 +1,17 @@
+class Foods
+{
+public static void main(String[] args)
+{
+String foodname="Paneer butter masala";
+int calories=400;
+String cuisinetype="Indian";
+double price=250.50;
+String vegetarian="true";
+
+System.out.println("Food name:"+foodname);
+System.out.println("Calories:"+calories);
+System.out.println("Cuisine type:"+cuisinetype);
+System.out.println("Vegetarian:"+vegetarian);
+System.out.println("Price:"+price);
+}
+}

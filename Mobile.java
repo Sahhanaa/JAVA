c2d7a3df8304749baaeefc8fc7cde6671c217987
Sahhana <sahhana13@gmail.com>
2025-12-20 15:36:16 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,17 @@
+class Mobile 
+{
+public static void main(String[] args)
+{
+String mobilename="iPhone 15 Pro";
+String mobilebrand="Apple";
+double screensize=6.1;
+short batterycapacity=3274;
+double price=134999.99;
+
+System.out.println(mobilename);
+System.out.println(mobilebrand);
+System.out.println(screensize);
+System.out.println(batterycapacity);
+System.out.println(price);
+}
+}

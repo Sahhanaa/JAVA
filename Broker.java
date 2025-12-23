291f19c7e75a1249c3b5992e3ada7350739df420
Sahhana <sahhana13@gmail.com>
2025-12-23 14:29:26 +0530

    Add files via upload

---
+++ Broker.java
@@ -0,0 +1,10 @@
+class Broker
+{
+public static void main(String[] args)
+{
+int automobilerate=750;
+double rate=0.15;
+double commission=automobilerate*rate;
+System.out.println("Commision rate:"+commission);
+}
+}
---
+++ Library.java
@@ -0,0 +1,19 @@
+class Library
+{
+public static void main(String[] args)
+{
+double mathsbooks=0.40;
+double englishbooks=0.20;
+double sciencebooks=0.30;
+double socialbooks=0.10;
+int totalbooks=1800;
+double totalmathsbooks=0.40*1800;
+double totalenglishbooks=0.20*1800;
+double totalsciencebooks=0.30*1800;
+double totalsocialbooks=0.10*1800;
+System.out.println("Number of maths book count:"+totalmathsbooks);
+System.out.println("Number of english book count:"+totalenglishbooks);
+System.out.println("Number of science book count:"+totalsciencebooks);
+System.out.println("Number of social book count:"+totalsocialbooks);
+}
+}

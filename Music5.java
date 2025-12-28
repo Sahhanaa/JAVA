10043f2662bee04c3e0b1f80ece2807291d2805e
Sahhana <sahhana13@gmail.com>
2025-12-28 17:18:18 +0530

    Add files via upload

---
+++ Music5.java
@@ -0,0 +1,322 @@
+import java.util.Scanner;
+class Music5
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Spotify welcomes you!");
+		System.out.println("1.A.R.RAHMAN");
+		System.out.println("2.ANIRUDH");
+		System.out.println("3.HARISH JEYARAJ");
+		System.out.println("4.ILLAIYARAJA");
+		System.out.println("5.U1");
+		System.out.print("Choose your option:");
+		int option=sc.nextInt();
+		
+		if(option==1)
+		{
+			System.out.println("1.Melody");
+			System.out.println("2.Kuthu");
+			System.out.println("3.Motivational song");
+			System.out.println("4.Sad");
+			System.out.print("Select your choise:");
+			int choise=sc.nextInt();
+			
+			if(choise==1)
+			{
+				System.out.println("Netru illadha Matram");
+				System.out.println("Enna Solla Pogirai");
+				System.out.println("Ennavale Adi Ennavale");
+                System.out.println("Malargale Malargale");
+
+			}
+			
+			else if(choise==2)
+			{
+				System.out.println("Thaiya Thaiya");
+				System.out.println("Mukkala Mukkabala");
+				System.out.println("Urvashi Urvashi");
+                System.out.println("Aathichudi");
+			}
+			else if(choise==3)
+			{
+				System.out.println("Vndhe mataram");
+				System.out.println("Oruvan oruvan mudhalali");
+				System.out.println("Shakthi kodu");
+                System.out.println("Kappal yeri poyachu");
+			}
+			else if(choise==4)
+			{
+				System.out.println("Poongatrile");
+				System.out.println("Marudhani");
+				System.out.println("Rasathi");
+                System.out.println("Minnale En vazhvin");
+				System.out.println("Oru poiyavathu sol");
+			}
+			else{
+				System.out.println("Your choise is not available");
+			}
+		}
+		
+		else if(option==2)
+		{
+			System.out.println("1.Melody");
+			System.out.println("2.Kuthu");
+			System.out.println("3.Motivational song");
+			System.out.println("4.Sad");
+			System.out.print("Select your choise:");
+			int choise=sc.nextInt();
+			
+			if(choise==1)
+			{
+				System.out.println("Neeyum naanum anbe");
+				System.out.println("Velicha poove");
+				System.out.println("Poo indru neeyaga");
+                System.out.println("Nee partha vizhigal");
+
+			}
+			
+			else if(choise==2)
+			{
+				System.out.println("Vathi raid");
+				System.out.println("Naa ready");
+				System.out.println("Badass");
+                System.out.println("Vandha edam");
+			}
+			else if(choise==3)
+			{
+				System.out.println("Velai illa pattathari");
+				System.out.println("Boomi enaa suthudhe");
+				System.out.println("Ethir neechal");
+                System.out.println("Syrviva");
+			}
+			else if(choise==4)
+			{
+				System.out.println("Porkanda singam");
+				System.out.println("Kanave kanave");
+				System.out.println("Kannana kanne");
+                System.out.println("Jodi nilave");
+			}
+			else{
+				System.out.println("Your choise is not available");
+			}
+		}
+		
+		else if(option==3)
+		{
+			System.out.println("1.Melody");
+			System.out.println("2.Kuthu");
+			System.out.println("3.Sad");
+			System.out.print("Select your choise:");
+			int choise=sc.nextInt();
+			
+			if(choise==1)
+			{
+				System.out.println("Unnale");
+				System.out.println("Annul maele");
+				System.out.println("Yedho ondru");
+                System.out.println("Paartha mudhal");
+				System.out.println("Vaseegara");
+			}
+			
+			else if(choise==2)
+			{
+				System.out.println("Tirunelveli halwa da");
+				System.out.println("Aradi katre");
+				System.out.println("Soda bottle");
+                System.out.println("Aruva meesai");
+			}
+			else if(choise==3)
+			{
+				System.out.println("Venmathiye");
+				System.out.println("Oh maname");
+				System.out.println("Yamma yamma");
+                System.out.println("Othayile");
+			}
+			else{
+				System.out.println("Your choise is not available");
+			}
+		}
+		
+		else if(option==4)
+		{
+			System.out.println("1.Melody");
+			System.out.println("2.Kuthu");
+			System.out.println("3.Sad");
+			System.out.print("Select your choise:");
+			int choise=sc.nextInt();
+			
+			if(choise==1)
+			{
+				System.out.println("Oru kili uruguthu");
+				System.out.println("Kuzhaloodham kannanuku");
+				System.out.println("Ennai thottu");
+                System.out.println("Raja raja cholan");
+			}
+			
+			else if(choise==2)
+			{
+				System.out.println("Ooruvittu ooruvandhu");
+				System.out.println("Nooru varusham");
+				System.out.println("Aasai nooruvagai");
+                System.out.println("Aasai athigam vachu");
+			}
+			else if(choise==3)
+			{
+				System.out.println("Kanne kalaimane");
+				System.out.println("Thene thenpandi neeye");
+				System.out.println("Paadi parandha killi");
+                System.out.println("Nilave vaa");
+			}
+			else{
+				System.out.println("Your choise is not available");
+			}
+		}
+		
+		else if(option==5)
+		{
+			System.out.println("1.Melody");
+			System.out.println("2.Kuthu");
+			System.out.println("3.Motivational song");
+			System.out.println("4.Sad");
+			System.out.print("Select your choise:");
+			int choise=sc.nextInt();
+			
+			if(choise==1)
+			{
+				System.out.println("Aathadi manasuthan");
+				System.out.println("Chinna sirisu");
+				System.out.println("Venmeggam");
+                System.out.println("Iyyayyyo");
+
+			}
+			
+			else if(choise==2)
+			{
+				System.out.println("Machi open the bottle");
+				System.out.println("Saroja saman nikalo");
+				System.out.println("No money no honey");
+                System.out.println("Villaiyadu mankatha");
+			}
+			else if(choise==3)
+			{
+				System.out.println("Eshirthu nill");
+				System.out.println("Oru nalil");
+				System.out.println("Nimirnthu nil");
+                System.out.println("Ungakkule mirugam");
+			}
+			else if(choise==4)
+			{
+				System.out.println("Kadhal valarthen");
+				System.out.println("Yedhi ondru ennai");
+				System.out.println("Pogadhe");
+                System.out.println("Ninaithu ninaithu");
+			}
+			else{
+				System.out.println("Your choise is not available");
+			}
+		}
+		
+		else
+		{
+			System.out.println("Thank you for using me");
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

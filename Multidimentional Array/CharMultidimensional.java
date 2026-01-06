7a96004c030fd738e097c6889b4c1136007f63d9
Sahhana <sahhana13@gmail.com>
2026-01-06 22:59:06 +0530

    Add files via upload

---
+++ Multidimentional Array/CharMultidimensional.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class CharMultidimensional
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		Scanner sc1 = new Scanner(System.in);
+		System.out.print("Enter the Row value :: ");
+		int row = sc.nextInt();
+		System.out.print("Enter the Column value :: ");
+		int col = sc.nextInt();
+		
+		char a[][] = new char [row][col];
+		
+		for(int i = 0;i<a.length;i++)
+		{
+			for(int j = 0;j<a[i].length;j++)
+			{
+				System.out.print("Enter the value of "+ i +","+ j +" index :: ");
+				a[i][j]= sc1.next().charAt(0);
+			}
+		}
+		
+		for(int i = 0;i<a.length;i++)
+		{
+			for(int j = 0;j<a[i].length;j++)
+			{
+				System.out.print(a[i][j]+ "  ");
+			}
+				System.out.println("");
+		}
+	}
+}
---
+++ Multidimentional Array/MarksMultidimensional.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class MarksMultidimensional
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Row value :: ");
+		int row = sc.nextInt();
+		System.out.print("Enter the Column value :: ");
+		int col = sc.nextInt();
+		
+		int a[][] = new int [row][col];
+		
+		for(int i = 0;i<a.length;i++)
+		{
+			for(int j = 0;j<a[i].length;j++)
+			{
+				System.out.print("Enter the value of "+ i +","+ j +" index :: ");
+				a[i][j]= sc.nextInt();
+			}
+		}
+		
+		for(int i = 0;i<a.length;i++)
+		{
+			for(int j = 0;j<a[i].length;j++)
+			{
+				System.out.print(a[i][j]+ "  ");
+			}
+				System.out.println("");
+		}
+	}
+}

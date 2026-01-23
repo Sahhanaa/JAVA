bc8ae31a80457de0ca7720dc15dba370ac4d173d
Sahhana <sahhana13@gmail.com>
2026-01-23 07:20:47 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,27 @@
+class BubbleSort
+{
+	public static void main(String[] args)
+	{
+		int arr[]={5,1,4,2,8,3};
+		int nop=0;
+		for(int i=0;i<arr.length-1;i++)
+		{
+			for(int j=0;j<arr.length-i-1;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+			   nop++;
+		}
+		System.out.println("Sorted array: ");
+        for (int i = 0; i < arr.length; i++)
+		{
+            System.out.print(arr[i] + " ");
+		}
+		System.out.println("\nNumber of passes:"+nop);
+	}
+}
